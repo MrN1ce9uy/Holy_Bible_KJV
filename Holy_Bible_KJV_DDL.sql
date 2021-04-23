5ab144c32a2334f6b79186a7bdf77f92ef9f4ee6
MrN1ce9uy <35491501+MrN1ce9uy@users.noreply.github.com>
2021-04-23 06:36:20 -0500

    Add files via upload

---
+++ Holy_Bible_KJV_DDL.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE Holy_Bible_KJV;
+
+USE Holy_Bible_KJV;
+
+CREATE TABLE book (
+	book_id char(3) PRIMARY KEY NOT NULL,
+	book_title varchar(15) NOT NULL,
+	testament_id char(2) NOT NULL
+	);
+
+CREATE TABLE testament (
+	testament_id char(2) PRIMARY KEY NOT NULL,
+	testament char(13) NOT NULL
+	);
+
+CREATE TABLE verse (
+	book_id char(3) NOT NULL,
+	chapter_id char(3) NOT NULL,
+	verse_id varchar(3) NOT NULL,
+	testament_id char(2) NOT NULL,
+	verse_text text NOT NULL,
+	PRIMARY KEY (book_id, chapter_id, verse_id)
+	);
+
+
